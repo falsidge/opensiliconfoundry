e7a40070df64be6781e8387b9ac9b6764ca7d8d8
falsidge <tesetings@gmail.com>
2019-04-17 14:06:39 -0700

    first commit. title screen

--- main.py
+++ main.py
@@ -10,24 +10,18 @@ screen = pygame.display.set_mode(size)
 title_screen = pygame.image.load("images/sf_title.png")
 title_screen_rect = title_screen.get_rect()
 
-ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
+x
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             running = False
-        elif event.type == pygame.VIDEORESIZE:
+     
 
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
 
     screen.fill(black)
     screen.blit(title_screen, title_screen_rect)
-    screen.blit(ball, ballrect)
+
     pygame.display.flip()
     pygame.time.delay(100)  
